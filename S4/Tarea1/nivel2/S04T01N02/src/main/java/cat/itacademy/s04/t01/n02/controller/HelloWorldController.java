dc78f5d4b920dbbeb477220e10276714bc00dbaf
Fer_ <fg.roglia@gmail.com>
2025-02-27 22:40:45 +0100

    Optimizando el ejercicio nivel2

--- S4/Tarea1/nivel2/S04T01N02/src/main/java/cat/itacademy/s04/t01/n02/controller/HelloWorldController.java
+++ S4/Tarea1/nivel2/S04T01N02/src/main/java/cat/itacademy/s04/t01/n02/controller/HelloWorldController.java
@@ -1,18 +1,22 @@
 package cat.itacademy.s04.t01.n02.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/HelloWorld")
 public class HelloWorldController {
 
-    @GetMapping
+    @GetMapping("/HelloWorld")
     public String hello(@RequestParam(value = "name", defaultValue = "UNKNOWN") String name) {
         return String.format("Hola %s!. Estás ejecutando un proyecto Maven", name);
 
+    }
+
+    @GetMapping({"/HelloWorld2", "/HelloWorld2/{name}"})
+    public String hello2(@PathVariable(required = false) String name) {
+        if (name == null) {
+            name = "UNKNOWN";
+        }
+        return String.format("Hola, %s!. Estás ejecutando un proyecto Maven", name);
     }
 
 }
